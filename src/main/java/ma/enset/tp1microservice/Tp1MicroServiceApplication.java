6d9734b5c30e625938938050b024074e8e5d6a74
wail-10 <koundiwail02@gmail.com>
2024-10-11 22:03:53 +0100

    first commit

---
+++ src/main/java/ma/enset/tp1microservice/Tp1MicroServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.tp1microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1MicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1MicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/enset/tp1microservice/Tp1MicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.tp1microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1MicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
